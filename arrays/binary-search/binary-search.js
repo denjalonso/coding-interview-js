5a99da8a1470f0088162078181224abf65826c12
Romén Rodríguez <romenrg@gmail.com>
2015-12-07 19:48:34 +0000

    Refactoring the 3 more complicated functions: 1. _isSearchKeyInTheLeftHalf (boolean); 2. _leftHalfProcessing and 3. _leftHalfProcessing

--- arrays/binary-search/binary-search.js
+++ arrays/binary-search/binary-search.js
@@ -1,4 +1,21 @@
 var NOT_FOUND = -1;
+
+function _isSearchKeyInTheLeftHalf(sortedNumbers, searchKey, indexInTheMiddle) {
+    return sortedNumbers[indexInTheMiddle] > searchKey;
+}
+
+function _leftHalfProcessing(sortedNumbers, searchKey, indexInTheMiddle) {
+    return binarySearch(sortedNumbers.slice(0, indexInTheMiddle), searchKey)
+}
+
+function _rightHalfProcessing(sortedNumbers, searchKey, indexInTheMiddle) {
+    var returnedIndex = binarySearch(sortedNumbers.slice(indexInTheMiddle, sortedNumbers.length), searchKey);
+    if (returnedIndex !== NOT_FOUND)
+        return indexInTheMiddle + returnedIndex;
+    else
+        return NOT_FOUND;
+}
+
 function binarySearch (sortedNumbers, searchKey) {
     if (sortedNumbers.length === 0) {
         return NOT_FOUND;
@@ -7,15 +24,11 @@ function binarySearch (sortedNumbers, searchKey) {
         return 0;
     } else if(sortedNumbers.length !== 1) {
         var indexInTheMiddle = parseInt(sortedNumbers.length / 2);
-        if(sortedNumbers[indexInTheMiddle] > searchKey) {
-            return binarySearch(sortedNumbers.slice(0, indexInTheMiddle), searchKey);
+        if(_isSearchKeyInTheLeftHalf(sortedNumbers, searchKey, indexInTheMiddle)) {
+            return _leftHalfProcessing(sortedNumbers, searchKey, indexInTheMiddle);
         }
         else {
-            var returnedIndex = binarySearch(sortedNumbers.slice(indexInTheMiddle, sortedNumbers.length), searchKey);
-            if (returnedIndex !== NOT_FOUND)
-                return indexInTheMiddle + returnedIndex;
-            else
-                return NOT_FOUND;
+            return _rightHalfProcessing(sortedNumbers, searchKey, indexInTheMiddle);
         }
     } else {
         return NOT_FOUND;
