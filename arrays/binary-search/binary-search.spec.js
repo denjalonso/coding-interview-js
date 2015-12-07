4d5b4a51e29739e851fd518937216e268e5718ec
Romén Rodríguez <romenrg@gmail.com>
2015-12-07 18:37:40 +0000

    Adding a more complete test, with several array elements, one of them being the search index

--- arrays/binary-search/binary-search.spec.js
+++ arrays/binary-search/binary-search.spec.js
@@ -11,4 +11,7 @@ describe("binary-search", function () {
     it("should return -1 if the array length is 1 and the search key doesn't match", function () {
         expect(binarySearch([2], 1)).to.equal(-1);
     });
+    it("should return 2 when looking for 30 in the array [10, 20, 30, 40]", function () {
+        expect(binarySearch([10, 20, 30, 40], 30)).to.equal(2);
+    });
 });
