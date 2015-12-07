8c396677a43bc2fd2d4ed1048cc620c0740c58e2
Romén Rodríguez <romenrg@gmail.com>
2015-12-07 19:09:38 +0000

    Binary search working for the first half

--- arrays/binary-search/binary-search.js
+++ arrays/binary-search/binary-search.js
@@ -4,6 +4,11 @@ function binarySearch (sortedNumbers, searchKey) {
     }
     if (sortedNumbers.length === 1 && sortedNumbers[0] === searchKey) {
         return 0;
+    } else if(sortedNumbers.length !== 1) {
+        var indexInTheMiddle = sortedNumbers.length / 2;
+        if(sortedNumbers[indexInTheMiddle] > searchKey) {
+            return binarySearch(sortedNumbers.slice(0, indexInTheMiddle), searchKey);
+        }
     } else {
         return -1;
     }
--- arrays/binary-search/binary-search.spec.js
+++ arrays/binary-search/binary-search.spec.js
@@ -11,6 +11,9 @@ describe("binary-search", function () {
     it("should return -1 if the array length is 1 and the search key doesn't match", function () {
         expect(binarySearch([2], 1)).to.equal(-1);
     });
+    it("should return 0 when looking for 10 in the array [10, 20, 30, 40]", function () {
+        expect(binarySearch([10, 20, 30, 40], 10)).to.equal(0);
+    });
     it("should return 2 when looking for 30 in the array [10, 20, 30, 40]", function () {
         expect(binarySearch([10, 20, 30, 40], 30)).to.equal(2);
     });
